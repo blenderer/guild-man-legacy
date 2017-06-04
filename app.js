e346ae048bde9c1c48534d7effdd26228cf2723b
Eric Harrison <eric.h@rrison.io>
2017-06-04 16:41:51 -0400

    webpack and firebase

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-'use strict';
-
-let GuildManApp = require('./classes/App');
-let RandomCharacter = require('./classes/RandomCharacter');
+const GuildManApp = require('./classes/App');
+const RandomCharacter = require('./classes/RandomCharacter');
+const firebase = require('firebase');
 
 let app;
+
+
+const fb = firebase.initializeApp({
+  apiKey: 'AIzaSyC06X1CKO5K1h3YInUCyjnzblE0q1lwxLQ',
+  authDomain: 'guild-man.firebaseapp.com',
+  databaseURL: 'https://guild-man.firebaseio.com',
+  storageBucket: 'guild-man.appspot.com',
+  messagingSenderId: '134513670784'
+});
+
+const db = fb.database();
+const activeQuests = db.ref('/activeQuests/');
+
+activeQuests.once('value').then((snapshot) => {
+  console.log(snapshot.val());
+});
 
 let getGameState = Promise.reject('404');
 
 getGameState.then(response => {
+
   app = new GuildManApp(response.gameState);
 }).catch(error => {
   if (error === '404') {
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: "./app.js",
+  devtool: 'source-map',
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+};
